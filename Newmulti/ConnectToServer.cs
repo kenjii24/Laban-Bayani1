a3a9157dcfc193f790bfdc6558c9135e46dbaaa8
kenjii24 <97772400+kenjii24@users.noreply.github.com>
2022-01-15 11:18:38 +0800

    Add files via upload

---
+++ Newmulti/ConnectToServer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ConnectToServer : MonoBehaviourPunCallbacks
+{
+    public InputField usernameInput;
+    public Text buttonText;
+
+    public void OnClickConnect()
+    {   
+        if(usernameInput.text.Length >= 1)
+        {
+            PhotonNetwork.NickName = usernameInput.text;
+            PhotonNetwork.AutomaticallySyncScene = true;
+            buttonText.text = "Connecting...";
+            PhotonNetwork.ConnectUsingSettings();
+        }
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+        SceneManager.LoadScene("MultiplayerPanel");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    // Update is called once per f rame
+    void Update()
+    {
+        
+    }
+}
---
+++ Newmulti/LobbyManagerr.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+
+public class LobbyManagerr : MonoBehaviourPunCallbacks
+{
+
+    public InputField roomInputField;
+    public GameObject lobbyPanel;
+    public GameObject roomPanel;
+    public Text roomName;
+
+    public RoomItem roomItemPrefab;
+    List<RoomItem> roomItemsList = new List<RoomItem>();
+    public Transform contentObject;
+
+
+    public float timeBetwwenUpdates = 1.5f;
+    float nextUpdateTime;
+
+    public List<PlayerItem> playerItemsList = new List<PlayerItem>();
+    public PlayerItem playerItemPrefab;
+    public Transform playerItemParent;
+
+    public GameObject playbutton;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        PhotonNetwork.JoinLobby();
+    }
+
+    public void OnClickCreate()
+    {
+        if (roomInputField.text.Length >= 1)
+        {
+            PhotonNetwork.CreateRoom(roomInputField.text, new RoomOptions() {MaxPlayers = 2,BroadcastPropsChangeToAll=true });
+        }
+    }
+
+    public override void OnJoinedRoom()
+    {
+        lobbyPanel.SetActive(false);
+        roomPanel.SetActive(true);
+        roomName.text = "Room Name :" + PhotonNetwork.CurrentRoom.Name;
+        UpdatePlayerList();
+      
+    }
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        if (Time.time>=nextUpdateTime)
+        {
+            UpdateRoomList(roomList);
+            nextUpdateTime = Time.time + timeBetwwenUpdates;
+        }
+        
+    }
+
+    void UpdateRoomList(List<RoomInfo> list)
+    {
+        foreach (RoomItem item in roomItemsList)
+        {
+            Destroy(item.gameObject);
+        }
+        roomItemsList.Clear();
+
+        foreach (RoomInfo room in list)
+        {
+            RoomItem newRoom = Instantiate(roomItemPrefab, contentObject);
+            newRoom.SetRoomName(room.Name);
+            roomItemsList.Add(newRoom);
+
+        }
+
+    }
+    public void JoinRoom(string roomName)
+    {
+        PhotonNetwork.JoinRoom(roomName);
+    }
+
+    public void OnClickLeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+
+    public override void OnLeftRoom()
+    {
+        roomPanel.SetActive(false);
+        lobbyPanel.SetActive(true);
+    }
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+    }
+
+    void UpdatePlayerList()
+    {
+        foreach (PlayerItem item in playerItemsList)
+        {
+            Destroy(item.gameObject);
+        }
+        playerItemsList.Clear();
+
+        foreach (KeyValuePair<int,Player>player in PhotonNetwork.CurrentRoom.Players)
+        {
+            PlayerItem newplayerItem = Instantiate(playerItemPrefab, playerItemParent);
+            newplayerItem.SetPlayerInfo(player.Value);
+            playerItemsList.Add(newplayerItem);
+            if (player.Value==PhotonNetwork.LocalPlayer)
+            {
+                newplayerItem.ApplyLocalChanges();
+            }
+          
+
+        }
+    }
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        UpdatePlayerList();
+    }
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        UpdatePlayerList();
+    }
+    // Update is called once per frame
+    private void Update()
+    {
+        if (PhotonNetwork.IsMasterClient && PhotonNetwork.CurrentRoom.PlayerCount>=2)
+        {
+            playbutton.SetActive(true);
+        }
+        else
+        {
+            playbutton.SetActive(false);
+        }
+    }
+    public void OnClickPlayButton()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            PhotonNetwork.LoadLevel("MultiLevel1");
+            PhotonNetwork.AutomaticallySyncScene = true;
+            PhotonNetwork.CurrentRoom.IsOpen = false;
+            PhotonNetwork.CurrentRoom.IsVisible = false;
+
+        }
+    }
+}
